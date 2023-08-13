a03e51f30ef3cbf34bb8e34d53dc18bbeb265e81
drizzle-mizzle <drizzle-mizzle@hotmail.com>
2023-08-13 19:19:49 +0500

    Update ReactionsHandler.cs

--- Handlers/ReactionsHandler.cs
+++ Handlers/ReactionsHandler.cs
@@ -20,37 +20,51 @@ namespace DuckBot.Handlers
 
         private async Task HandleReactionAddedAsync(Cacheable<IUserMessage, ulong> rawMessage, Cacheable<IMessageChannel, ulong> discordChannel, SocketReaction reaction)
         {
-            if (reaction.User.Value is not SocketGuildUser user) return;
-            if (user.IsBot) return;
-
-            var message = await rawMessage.DownloadAsync();
-            if (message.Author.Id != _client.CurrentUser.Id) return;
-            if (message.Channel is not SocketGuildChannel channel) return;
-
-            string? emojiName = reaction.Emote?.Name;
-            if (string.IsNullOrWhiteSpace(emojiName)) return;
-
-            if (FREE_EMOJIS.Contains(emojiName))
+            try
             {
-                await user.AddRoleAsync(channel.Guild.Roles.First(r => r.Name == emojiName));
+                if (reaction.User.Value is not SocketGuildUser user) return;
+                if (user.IsBot) return;
+
+                var message = await rawMessage.DownloadAsync();
+                if (message.Author.Id != _client.CurrentUser.Id) return;
+                if (message.Channel is not SocketGuildChannel channel) return;
+
+                string? emojiName = reaction.Emote?.Name;
+                if (string.IsNullOrWhiteSpace(emojiName)) return;
+
+                if (FREE_EMOJIS.Contains(emojiName))
+                {
+                    await user.AddRoleAsync(channel.Guild.Roles.First(r => r.Name == emojiName));
+                }
+            }
+            catch (Exception e)
+            {
+                LogException(new[] { e });
             }
         }
 
         private async Task HandleReactionRemovedAsync(Cacheable<IUserMessage, ulong> rawMessage, Cacheable<IMessageChannel, ulong> discordChannel, SocketReaction reaction)
         {
-            if (reaction.User.Value is not SocketGuildUser user) return;
-            if (user.IsBot) return;
-
-            var message = await rawMessage.DownloadAsync();
-            if (message.Author.Id != _client.CurrentUser.Id) return;
-            if (message.Channel is not SocketGuildChannel channel) return;
-
-            string? emojiName = reaction.Emote?.Name;
-            if (string.IsNullOrWhiteSpace(emojiName)) return;
-
-            if (FREE_EMOJIS.Contains(emojiName))
+            try
             {
-                await user.RemoveRoleAsync(channel.Guild.Roles.First(r => r.Name == emojiName));
+                if (reaction?.User.Value is not SocketGuildUser user) return;
+                if (user.IsBot) return;
+
+                var message = await rawMessage.DownloadAsync();
+                if (message.Author.Id != _client.CurrentUser.Id) return;
+                if (message.Channel is not SocketGuildChannel channel) return;
+
+                string? emojiName = reaction.Emote?.Name;
+                if (string.IsNullOrWhiteSpace(emojiName)) return;
+
+                if (FREE_EMOJIS.Contains(emojiName))
+                {
+                    await user.RemoveRoleAsync(channel.Guild.Roles.First(r => r.Name == emojiName));
+                }
+            }
+            catch (Exception e)
+            {
+                LogException(new[] { e });
             }
         }
 
